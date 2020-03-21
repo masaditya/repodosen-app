14dbbfb2f8d9bee09e744a801d1b6b43d7b14cbe
masaditya <aeka489@gmail.com>
2020-03-21 20:59:44 +0700

    fix(screen/addRepo) : fixing file picker

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -23,4 +23,4 @@ export const rootReducer = (prevState, action) => {
                 ...prevState
             }
     }
-}
+}   
--- src/screen/AddRepo/index.js
+++ src/screen/AddRepo/index.js
@@ -1,22 +1,28 @@
 import React, {useState, Component} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View, Alert} from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
-import {Layout} from '@ui-kitten/components';
+import {
+  Layout,
+  Input,
+  Select,
+  RangeCalendar,
+  Calendar,
+  Popover,
+  Button,
+} from '@ui-kitten/components';
 import {styles} from '../../styles';
 import {Header} from '../../components/Header/Header';
 
-export default class AddRepoScreen extends Component {
-  state = {
-    fileObj: '',
-  };
+const AddRepoScreen = () => {
+  const [fileRepo, setFileRepo] = useState('');
 
-  uploadFileRepoHandler = async () => {
+  const uploadFileRepoHandler = async () => {
     try {
       const res = await DocumentPicker.pick({
         type: [DocumentPicker.types.allFiles],
       });
       console.log(res);
-      this.setState({fileObj: res});
+      setFileRepo(res);
     } catch (err) {
       if (DocumentPicker.isCancel(err)) {
         Alert.alert('Canceled');
@@ -27,84 +33,62 @@ export default class AddRepoScreen extends Component {
     }
   };
 
-  render() {
-    return (
-      <Layout>
-        <Header />
-        <Layout
+  const [expiredDate, setExpiredDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const data = [{text: 'Option 1'}, {text: 'Option 2'}, {text: 'Option 3'}];
+
+  const CalendarContent = () => (
+    <Calendar date={expiredDate} onSelect={setExpiredDate} />
+  );
+
+  const toggleCalendar = () => {
+    console.log(showCalendar);
+    setShowCalendar(!showCalendar);
+  };
+
+  return (
+    <Layout>
+      <Header />
+      <Layout
+        style={{
+          paddingHorizontal: '7%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text
           style={{
-            paddingHorizontal: '7%',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            color: '#bebebe',
+            fontWeight: 'bold',
+            fontSize: 24,
+            paddingVertical: 20,
           }}>
-          <Text
-            style={{
-              color: '#bebebe',
-              fontWeight: 'bold',
-              fontSize: 24,
-              paddingVertical: 20,
-            }}>
-            Sertifikasi
-          </Text>
-        </Layout>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => this.uploadFileRepoHandler()}>
-          <Text style={styles.buttonText}>Click Here To Pick File</Text>
-        </TouchableOpacity>
+          Sertifikasi
+        </Text>
       </Layout>
-    );
-  }
-}
+      <Layout style={styles.ph_15}>
+        <Input style={styles.mv_15} label="Nama Kegiatan" value={fileRepo} />
+        <Input style={styles.mv_15} label="No. Sertifikat" value={fileRepo} />
+        <Select style={styles.mv_15} label="Bidang Kompetensi" data={data} />
+        <Popover
+          visible={showCalendar}
+          onBackdropPress={toggleCalendar}
+          content={CalendarContent()}>
+          <Button status="basic" onPress={toggleCalendar}>
+            {expiredDate.toLocaleDateString()}
+          </Button>
+        </Popover>
+      </Layout>
+    </Layout>
+  );
+};
 
-// const AddRepoScreen = ({route}) => {
-//   // const [fileRepo, setFileRepo] = useState({});
+//
+//     -
+//     -
+//     - Jangka Waktu Berlaku
+//     - File Sertifikat
+//     - File Surat Tugas (Opsional)
 
-//   const uploadFileRepoHandler = async () => {
-//     try {
-//       const res = await DocumentPicker.pick({
-//         type: [DocumentPicker.types.allFiles],
-//       });
-//       setFileRepo(res);
-//       // this.setState({singleFileOBJ: res});
-//     } catch (err) {
-//       if (DocumentPicker.isCancel(err)) {
-//         Alert.alert('Canceled');
-//       } else {
-//         Alert.alert('Unknown Error: ' + JSON.stringify(err));
-//         throw err;
-//       }
-//     }
-//   };
-
-//   return (
-//     <Layout>
-//       <Header />
-//       <Layout
-//         style={{
-//           paddingHorizontal: '7%',
-//           flexDirection: 'row',
-//           justifyContent: 'space-between',
-//           alignItems: 'center',
-//         }}>
-//         <Text
-//           style={{
-//             color: '#bebebe',
-//             fontWeight: 'bold',
-//             fontSize: 24,
-//             paddingVertical: 20,
-//           }}>
-//           Sertifikasi
-//         </Text>
-//       </Layout>
-//       <TouchableOpacity
-//         activeOpacity={0.5}
-//         onPress={() => uploadFileRepoHandler()}>
-//         <Text style={styles.buttonText}>Click Here To Pick File</Text>
-//       </TouchableOpacity>
-//     </Layout>
-//   );
-// };
-
-// export default AddRepoScreen;
+export default AddRepoScreen;
